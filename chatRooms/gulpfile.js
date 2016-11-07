ab0d6768287636ff5aead5fd3714791bfc9ff167
wwervin72 <2411120974@qq.com>
2016-11-08 00:11:12 +0800

    chatRooms gulp

---
+++ chatRooms/gulpfile.js
@@ -0,0 +1,111 @@
+var gulp = require('gulp'),
+	sass = require('gulp-ruby-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	jshint = require('gulp-jshint'),
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'),
+	rename = require('gulp-rename'),
+	clean = require('gulp-clean'),
+	concat = require('gulp-concat'),
+	notify = require('gulp-notify'),
+	cache = require('gulp-cache'),
+	plumber = require('gulp-plumber'),
+	livereload = require('gulp-livereload'),
+	connect = require('gulp-connect');
+
+
+gulp.task('styles', function () {
+	return sass('./public/sass/*.scss', {style: 'expanded', sourcemap: true})
+		.pipe(plumber())
+		.pipe(autoprefixer({
+			browsers: ['last 2 version', 'safari 5', 'ie 6', 'opera 12.1', 'ios 6', 'android 4'],
+			cascade: true,//是否美化属性值 默认：true 像这样：
+			//-webkit-transform: rotate(45deg);
+            //        transform: rotate(45deg);
+            remove:true //是否去掉不必要的前缀 默认：true 
+		}))
+		.pipe(gulp.dest('./public/dist/css/'))
+		.pipe(minifycss())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('./public/dist/css/'))
+		.pipe(livereload())
+		.pipe(notify({message: 'styles task complete'}));
+});
+
+gulp.task('scripts', function () {
+	return gulp.src('./public/scripts/*.js')
+		.pipe(plumber())
+		.pipe(jshint('.jshintrc'))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('./public/dist/js/'))
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('./public/dist/js/'))
+		.pipe(livereload())
+		.pipe(notify({message: 'scripts task complete'}));
+});
+
+gulp.task('images', function () {
+	return gulp.src('./public/images/*')
+		.pipe(plumber({
+			errorHandler: function (err) {
+				console.log(err);
+				this.emit('end');
+			}	
+		}))
+		.pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('./public/dist/img/'))
+		.pipe(livereload())
+		.pipe(notify({message: 'images task complete'}));
+});
+
+gulp.task('connect', function () {
+	connect.server({
+	    name: 'chatRoom',
+	    root: './server.js',
+	    port: 3000,
+	    livereload: true
+	 });
+});
+
+// gulp.task('webserver', function () {
+// 	return gulp.src('server.js')
+// 		.pipe(webserver({
+// 			port: 3000,//端口
+// 			host: 'localhost',//域名
+// 			liveload: true,//实时刷新代码。不用f5刷新
+// 			directoryListing: {
+// 				path: 'server.js',
+// 				enable: true
+// 			}
+// 		}))
+// 		.pipe(notify({message: 'webserver task complete'}));
+// });
+// kill the webserver
+// var stream = gulp.src('app').pipe(webserver());
+// stream.emit('kill');
+
+gulp.task('clean', function () {
+	gulp.src(['./public/dist/css/', './public/dist/js/', './public/dist/img/'])
+		.pipe(clean());
+});
+
+gulp.task('clear', function (done) {
+	return cache.clearAll(done);
+});
+
+gulp.task('default', ['clean', 'clear'], function () {
+	gulp.start(['scripts', 'styles', 'images', 'connect']);
+});
+
+
+gulp.task('watch',['default'], function () {
+
+	gulp.watch('./public/sass/**/*.scss', ['styles']);
+	gulp.watch('./public/scripts/**/*.js', ['scripts']);
+	gulp.watch('./public/images/**/*', ['images']);
+	gulp.watch('./server.js', ['connect']);
+
+	livereload.listen();
+});
